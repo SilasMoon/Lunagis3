71d57bedec99c7b9d94fa75789cf1283e87e2659
SilasMoon <87944204+SilasMoon@users.noreply.github.com>
2025-12-01 16:24:33 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: process.env.BASE_URL || env.BASE_URL || '/Lunagis2/',
+    base: process.env.BASE_URL || env.BASE_URL || '/Lunagis3/',
     server: {
       port: 3000,
       host: '0.0.0.0',
